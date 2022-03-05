e53be401f96f7e54a1161244f6579188c475aae7
rohanpatel01 <rohanppatel01@gmail.com>
2022-03-05 03:12:21 -0600

    Final code that will be sent for review

--- mainProgram/mainProgram.ino
+++ mainProgram/mainProgram.ino
@@ -1,33 +1,4 @@
-/*
- * Issue log:
- * CheckMoisture is not logging the right values from water sensor
- * this also causes system to crash. See what is going on here.
- * 
- * 
- * Solution log:
- * I changed the print statement to come before the array++
- * This way I didn't log a value that didn't exist b/c we're
- * trying to access something ahead of what we have
- * 
- * ^ So far initialScan and checkMoisture work perfectly, 
- * now need to test wait feature
- * 
- * ***** NEED TO FIX SOMETHING WITH WAIT FEATURE
- * Wait feature works fine until 5+ iterations then it spams
- * 
- * 
- * Moved start = currentTime in wait method to inside the if statement
- * from outside the if statement. I think this will fix the spamming issue 
- * cuz that is a timing issue
- * 
- * Shubham said to try and limit the amount of memory and Int can use.
- * Look into U Int 24 8 16
- * Need std.h
- * 
- */
-
 #include <stdio.h>
-
 #include <MemoryFree.h>;
 
 #include <Array.h>
@@ -35,20 +6,11 @@
 #include <LiquidCrystal_I2C.h>
 #include <Stepper.h>
 
-
-
-
 int waterInputPin = A0;
 int buttonInputPin = 9;
-// initialization and variables related to it
 
-
-const int stepsPerRev = 2038;
-int motorSpeed = 10;
-int in1 = 8, in2 = 9, in3 = 10, in4 = 11;
-Stepper stepper (stepsPerRev, in1, in2, in3, in4);
 LiquidCrystal_I2C lcd (0x27, 16, 2);
-// variables for program
+
 int start;
 int waterVal = 0;
 int eventInterval = 1000;
@@ -87,8 +49,7 @@ void setup()
     lcd.backlight();
     lcd.setCursor(0, 0);
     lcd.print("Hello There!");
-    stepper.setSpeed(motorSpeed);
-    Serial.println("--------------------------------------------");
+    Serial.println("--------------");
     Serial.println("Starting System");
    
 } // end of setup
@@ -102,8 +63,6 @@ void loop()
     checkMoisture();
     
     waitBeforeNextScan();
-    
-    //motorControl();
 }
 
 void checkMoisture()
@@ -111,12 +70,12 @@ void checkMoisture()
    if(millis() - start >= eventInterval && timePassed < totalScanLength && shouldScan)
    {
       digitalWrite(leftLED, LOW);
-      
-      // turn on current LED
       digitalWrite(middleLED, HIGH);
-
       digitalWrite(rightLED, LOW);
-      
+
+      // this will pass data from water sensor into array
+      // if time is almost up we will continue getting the values
+      // when time is up we will get the final value in the else statement
       if(timePassed < totalScanLength - 1)
       {
          timePassed++;
@@ -124,29 +83,19 @@ void checkMoisture()
           highestAverageMoisture[arrayIndex] = waterVal;
           Serial.println(highestAverageMoisture[arrayIndex]);
           arrayIndex++;
-
-         Serial.println("Free Memory"); 
-         Serial.println(freeMemory());
-         
-         start = millis();
+          
+          start = millis();
       }else
       {
-        // get last value
-        //timePassed++;
-        
         highestAverageMoisture[arrayIndex] = waterVal;
         Serial.println(highestAverageMoisture[arrayIndex]);
 
-        // this might be the issue
-        //arrayIndex++;
-
-        // find average
-            for(int x = 0; x < arrayLength; x++)
-            {
-                averageSum += highestAverageMoisture[x];
-            }
-                
-
+        // find average of moisture
+        for(int x = 0; x < arrayLength; x++)
+        {
+            averageSum += highestAverageMoisture[x];
+        }
+        
         Serial.print("Average : ");
         Serial.println(averageSum / arrayLength);
 
@@ -168,7 +117,6 @@ void checkMoisture()
         wait = true;
 
         start = millis();
-        
       }
    }
 }
@@ -177,23 +125,21 @@ void waitBeforeNextScan()
 {
    if(millis() - start >= eventInterval && timePassed < timeTillNextScan && wait)
    {
-      // turn off previous LED
       digitalWrite(middleLED, LOW);
-    
-      // turn on current LED
       digitalWrite(rightLED, HIGH);
 
-      // routine before last value
+      // this will pass data from water sensor into array
+      // if time is almost up we will continue getting the values
+      // when time is up we will get the final value in the else statement
       if(timePassed < timeTillNextScan - 1)
       {
-        
         timePassed++;
         Serial.println(timePassed);
         start = millis();
         
-      }else   // the last value and when scan is over
+      }else   
       {
-        // last value
+        
         timePassed++;
         Serial.println(timePassed);
         
@@ -213,7 +159,8 @@ void createLevelsOfMoisture(int average)
     threeFourthSaturation = average * 0.75;
     halfSaturation = average / 2;
     dryBoi = average / 5;    
- 
+
+    // values for next phase
     timePassed = 0; 
     shouldScan = true;
     start = millis();
@@ -222,7 +169,6 @@ void createLevelsOfMoisture(int average)
 
 int initialSetup()
 {
-  
   if(digitalRead(buttonInputPin) == HIGH && initialSetupStep == 0)
   {
     digitalWrite(leftLED, HIGH);
@@ -239,8 +185,7 @@ int initialSetup()
   if(millis() - start >= eventInterval && timePassed < totalScanLength && initialSetupStep == 1)
   {
      digitalWrite(leftLED, HIGH);
-
-     
+      
      // this will pass data from water sensor into array
      // if time is almost up we will continue getting the values
      // when time is up we will get the final value in the else statement
@@ -289,10 +234,8 @@ int initialSetup()
   }
 }// end of initialWaterReading
 
-
 void  clearArray()
 {
-    
     for(int x = 0; x < arrayLength; x++)
     {
       highestAverageMoisture[x] = 0;
@@ -303,14 +246,4 @@ void  clearArray()
     
     Serial.println("array cleared");
      
-}
-
-
-void motorControl()
-{
-  
-  Serial.println("clockwise full");
-  stepper.step(stepsPerRev);
-  delay(500);
-  
 }
